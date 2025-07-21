e47c1812b2dff0ce51ec1db142e7736cac270dd1
Fostar74 <emojiivasilev@gmail.com>
2025-07-20 17:39:13 -0700

    Create auth.js

---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const authRoutes = require("./routes/auth");
+
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Colonize-Mars Backend is running.");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
