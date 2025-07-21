50b1d9f86b736129f2ce6351fe9ead38ebf66c4d
Fostar74 <emojiivasilev@gmail.com>
2025-07-20 20:10:02 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LoginRegister from "./LoginRegister";
 
 function App() {
   return (
-    <div>
-      <LoginRegister />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginRegister />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        {/* В бъдеще може да добавим: <Route path="/game" element={<Game />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
