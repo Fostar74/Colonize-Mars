160d5925e458aed832452ed8f53ccc87fa865482
Fostar74 <emojiivasilev@gmail.com>
2025-07-20 19:25:53 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
+import React from "react";
+import LoginRegister from "./LoginRegister";
 
 function App() {
   return (
-    <div className="App">
-      <h1>🚀 Welcome to Colonize Mars</h1>
-      <p>Prepare your strategy, build your colony, and conquer the Red Planet.</p>
-      <button onClick={() => alert("Start Game coming soon!")}>Play Now</button>
+    <div>
+      <LoginRegister />
     </div>
   );
 }
