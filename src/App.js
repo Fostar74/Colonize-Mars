3b8f444886f1cadc79283a6ae68a412e696dd053
Fostar74 <emojiivasilev@gmail.com>
2025-07-26 09:21:36 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import LoginRegister from "./pages/LoginRegister";
 import Game from "./pages/Game";
-import map from "./pages/map"; // ✅ lowercase import
+import Map from "./pages/Map"; // ✅ lowercase import
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginRegister />} />
         <Route path="/game" element={<Game />} />
-        <Route path="/map" element={<map />} />
+        <Route path="/Map" element={<Map />} />
       </Routes>
     </Router>
   );
